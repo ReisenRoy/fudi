3d8114f8c94fdafb99163f168bda6345104076de
Kristina <48881245+ReisenRoy@users.noreply.github.com>
2019-10-06 19:55:57 +0300

    feat: add modal sign-up/log-in form

--- js/app.js
+++ js/app.js
@@ -1,8 +1,35 @@
-(function() {
-  var button = document.getElementById('toggle-menu');
-  button.addEventListener('click', (e) => {
-    e.preventDefault();
-    var menu = document.getElementById('extra-menu');
-    menu.classList.toggle('hidden');
-  });
-})();
+const elements = {
+  menuBtn: document.getElementById('toggle-menu'),
+  menu: document.getElementById('extra-menu'),
+  loginBtn: document.getElementById('login-btn'),
+  changeForm: document.getElementById('change-form'),
+  loginForm: document.querySelector('.form__log-in'),
+  signupForm: document.querySelector('.form__sign-up'),
+  modal: document.getElementById('modal'),
+  closeModal: document.getElementById('close')
+};
+
+elements.menuBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  elements.menu.classList.toggle('hidden');
+});
+
+elements.loginBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  elements.modal.classList.toggle('hidden');
+});
+
+elements.changeForm.addEventListener('click', (e) => {
+  e.preventDefault();
+  elements.loginForm.classList.add('hidden');
+  elements.signupForm.classList.remove('hidden');
+  elements.changeForm.classList.add('hidden');
+});
+
+elements.closeModal.addEventListener('click', (e) => {
+  e.preventDefault();
+  elements.modal.classList.add('hidden');
+  elements.changeForm.classList.remove('hidden');
+  elements.loginForm.classList.remove('hidden');
+  elements.signupForm.classList.add('hidden');
+});
